14a8737f9748c10c568f41fed843de56e7e1700f
jaenek <jaenek@protonmail.com>
2020-05-07 06:36:24 +0200

    Add connection drawing

--- Assets/Scripts/CircuitController.cs
+++ Assets/Scripts/CircuitController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CircuitController : MonoBehaviour
 {
     public static CircuitController instance;
+    public Material connectionMaterial;
 
     void Awake()
     {
@@ -14,11 +16,16 @@ public class CircuitController : MonoBehaviour
     private class Node
     {
         public List<ConnectionClass> connections = new List<ConnectionClass>();
+        public List<LineRenderer> lines = new List<LineRenderer>();
 
         public Node(ConnectionClass conn)
         {
            	connections.Add(conn);
 			conn.connected = true;
+        }
+        public LineRenderer LastLine()
+        {
+            return lines[lines.Count-1];
         }
     }
 
@@ -29,20 +36,47 @@ public class CircuitController : MonoBehaviour
         return nodes[nodes.Count-1];
     }
 
-    public void AddConnection(ItemClass element)
+    private void DrawLine(Node node, Vector3 pos1, Vector3 pos2)
     {
-		var conn = element.GetFreeConnection();
+        node.lines.Add(new GameObject("Line" + (node.lines.Count + 1)).AddComponent<LineRenderer>());
+        node.LastLine().material = connectionMaterial;
+        node.LastLine().positionCount = 2;
+        node.LastLine().startWidth = 0.02f;
+        node.LastLine().endWidth = 0.02f;
+        node.LastLine().useWorldSpace = true;
+        node.LastLine().numCapVertices = 5;
+        node.LastLine().SetPosition(0, pos1);
+        node.LastLine().SetPosition(1, pos2);
+    }
+
+    private void DrawNode(Node node)
+    {
+        if (node.connections.Count == 2)
+        {
+            DrawLine(node, node.connections[0].transform.position, node.connections[1].transform.position);
+        }
+        else
+        {
+            Vector3 middle = (node.connections[0].transform.position + node.connections[1].transform.position)/2;
+            DrawLine(node, middle, node.connections[node.connections.Count-1].transform.position);
+        }
+    }
+
+    public void AddConnection(ItemClass element, bool newConnection)
+    {
+		var conn = element.GetClosestConnection();
 		if (conn != null)
 		{
 			if (nodes.Count > 0)
 			{
-				if (LastNode().connections.Contains(conn))
+				if (LastNode().connections.Contains(conn) || newConnection)
 				{
         			nodes.Add(new Node(conn));
 				}
 				else {
         			LastNode().connections.Add(conn);
 					conn.connected = true;
+                    DrawNode(LastNode());
 				}
 			}
 			else
