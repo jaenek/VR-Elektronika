17c26b4dc3c658c40b5eb4f70aff982ffcafec9c
jaenek <jaenek@protonmail.com>
2020-05-07 06:38:56 +0200

    Changed GetFreeConnection to GetClosestConnection

--- Assets/Scripts/ItemClass.cs
+++ Assets/Scripts/ItemClass.cs
@@ -11,15 +11,38 @@ public class ItemClass : MonoBehaviour
     public Vector3 originTransform = Vector3.down;
 	public List<ConnectionClass> connections;
 
-	public ConnectionClass GetFreeConnection()
+	public ConnectionClass GetClosestConnection()
 	{
-		foreach (var conn in connections)
-		{
-			if (!conn.connected)
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 4))
+        {
+			var closest = connections[0];
+			var closestDistance = Vector3.Distance(connections[0].transform.position, hit.point);
+			foreach (var conn in connections.GetRange(1, connections.Count-1))
 			{
-				return conn;
+				var dist = Vector3.Distance(conn.transform.position, hit.point);
+				if (dist < closestDistance)
+				{
+					closest = conn;
+				}
+				closestDistance = dist;
+			}
+
+			return closest;
+        }
+		else
+		{
+			foreach (var conn in connections)
+			{
+				if (!conn.connected)
+				{
+					return conn;
+				}
 			}
 		}
+
 		return null;
 	}
 }
